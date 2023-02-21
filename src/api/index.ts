64169e7808361e7bfefeea9b8368c1d862f40fc8
Samuel Silva <samuelsilvawb@gmail.com>
2023-02-21 23:59:10 +0000

    refactor: add proper types to each pokemon response

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,26 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { fakePokemonDetailData, fakePokemonListing } from "../mocks/data";
+
+export type PokemonList = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: { name: string; url: string }[];
+};
+
+export type PokemonDetails = {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  types: { slot: number; type: { name: string; url: string } }[];
+  sprites: { front_default: string };
+};
 
 export const pokemonApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
   endpoints: (builder) => {
     return {
-      pokemonList: builder.query<typeof fakePokemonListing, void>({
+      pokemonList: builder.query<PokemonList, void>({
         query: () => {
           return {
             url: "pokemon",
@@ -13,10 +28,7 @@ export const pokemonApi = createApi({
           };
         },
       }),
-      pokemonDetails: builder.query<
-        typeof fakePokemonDetailData,
-        { name: string }
-      >({
+      pokemonDetails: builder.query<PokemonDetails, { name: string }>({
         query: ({ name }) => {
           return {
             url: "pokemon/" + name,
