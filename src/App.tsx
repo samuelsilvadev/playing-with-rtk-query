4fa645e7db76fbe654259a946fd495897d002940
Samuel Silva <samuelsilvawb@gmail.com>
2023-02-21 00:13:48 +0000

    feat: prepare project and display fake data

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { fakePokemonDetailData, fakePokemonListing } from "./mocks/data";
+import "./App.css";
 
 function App() {
+  const [selectedPokemon, selectPokemon] = React.useState<string | undefined>();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>My Pokedex</h1>
       </header>
-    </div>
+      <main>
+        {selectedPokemon ? (
+          <>
+            <PokemonDetails pokemonName={selectedPokemon} />
+            <button onClick={() => selectPokemon(undefined)}>back</button>
+          </>
+        ) : (
+          <PokemonList onPokemonSelected={selectPokemon} />
+        )}
+      </main>
+    </>
+  );
+}
+
+type PokemonListProps = {
+  onPokemonSelected: (pokemonName: string | undefined) => void;
+};
+
+function PokemonList({ onPokemonSelected }: PokemonListProps) {
+  const data = fakePokemonListing;
+
+  return (
+    <article>
+      <h2>Overview</h2>
+      <ol start={1}>
+        {data.results.map((pokemon) => (
+          <li key={pokemon.name}>
+            <button onClick={() => onPokemonSelected(pokemon.name)}>
+              {pokemon.name}
+            </button>
+          </li>
+        ))}
+      </ol>
+    </article>
+  );
+}
+
+type PokemonDetailsProps = {
+  pokemonName: string;
+};
+
+function PokemonDetails({ pokemonName }: PokemonDetailsProps) {
+  const data = fakePokemonDetailData;
+
+  return (
+    <article>
+      <h2>{data.name}</h2>
+      <img src={data.sprites.front_default} alt={data.name} />
+      <ul>
+        <li>id: {data.id}</li>
+        <li>height: {data.height}</li>
+        <li>weight: {data.weight}</li>
+        <li>
+          types:
+          {data.types.map((item) => item.type.name)}
+        </li>
+      </ul>
+    </article>
   );
 }
 
